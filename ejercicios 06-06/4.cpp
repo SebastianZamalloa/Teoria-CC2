56a18f332ae12f6423b606e1c64f23abaf180d8c
SebastianZamalloa <104155286+SebastianZamalloa@users.noreply.github.com>
2022-06-07 19:54:54 -0500

    Add files via upload

--- ejercicios 06-06/4.cpp
+++ ejercicios 06-06/4.cpp
@@ -1,1 +1,38 @@
+#include "iostream"
+#include "vector"
+using namespace std;
 
+template<typename T>
+class stack
+{
+    private:
+        vector<T> myStack;
+    public:
+        stack(){}
+        ~stack(){}
+        bool empty(){if(myStack.size()==0){return true;}return false; }
+        void push(const T &item){myStack.push_back(item);}
+        void pop(){myStack.pop_back();}
+        void print()
+        {
+            if(!this->empty())
+            {
+                for(auto i = myStack.begin();i != myStack.end();i++)
+                    cout<<*i<<" ";
+            }
+            else
+                cout<<"Pila vacia"<<endl;
+        }
+        T& top(){return myStack[myStack.size()-1];}
+};
+int main()
+{
+    stack<int> miPila;
+    miPila.print();
+    for(int i = 0;i<7;i++)
+        miPila.push(i+1);
+    miPila.pop();
+    miPila.pop();
+    miPila.top() = 1;
+    miPila.print();
+}
