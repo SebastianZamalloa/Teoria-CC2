e68dd0a2257aa9a5cf2bdb66ff74e972c930be87
SebastianZamalloa <104155286+SebastianZamalloa@users.noreply.github.com>
2022-06-07 20:35:26 -0500

    Add files via upload

--- ejercicios 06-06/4.cpp
+++ ejercicios 06-06/4.cpp
@@ -27,12 +27,20 @@ class stack
 };
 int main()
 {
+    cout<<"\nPila en C++:\n";
+    cout<<"\nCreando pila...\n\nImprimiendo pila recien creada...\n";
     stack<int> miPila;
     miPila.print();
+    cout<<"La pila esta vacia, debemos ingresar numeros en ella...\n\nIngresando numeros del 1 al 7...\nImprimiendo...\n";
     for(int i = 0;i<7;i++)
         miPila.push(i+1);
+    miPila.print();cout<<endl;
+    cout<<"\nImpresion exitosa...\nEliminando los dos ultimos elementos...\n";
     miPila.pop();
     miPila.pop();
+    miPila.print();
+    cout<<"\nAgregando como ultimo elemento '1'...\n";
     miPila.top() = 1;
     miPila.print();
+    cout<<"\nFin del programa"<<endl<<endl;
 }
--- ejercicios 06-06/5.cpp
+++ ejercicios 06-06/5.cpp
@@ -3,44 +3,51 @@
 #include <map>
 using namespace std;
 
-class Graph 
+class grafo 
 {
     protected :
-        map <int , vector <int > > outgoing ;
+        map <int,vector <int>> outgoing;
     public :
-    Graph ( const vector <int > & startPoints , const vector <int > &
-    endPoints );
-    int numOutgoing ( const int nodeID ) const ;
-    const vector <int > & adjacent ( const int nodeID ) const ;
+    grafo(const vector <int> &inicio,const vector<int > &final)
+    {
+        if(inicio.size() != final.size()) 
+            throw invalid_argument ("Las listas de inicio y final tienen diferente cantidad de elementos");
+        for(unsigned i = 0; i<inicio.size(); i++) 
+        {
+            int in = inicio[i];
+            int fi = final[i];
+            outgoing[in].push_back(fi);
+            outgoing[fi];
+        }
+    }
+    int numOutgoing(const int ID_nodo) const{return adyacente(ID_nodo).size();}
+    const vector <int> &adyacente(const int ID_nodo) const
+    {
+        map<int,vector<int>>::const_iterator i = outgoing.find(ID_nodo);
+        if(i == outgoing.end()) 
+            throw invalid_argument("Id de nodo invalido");
+        return i->second;
+    }
 };
-
-Graph :: Graph ( const vector <int > & startPoints , const vector <int > &endPoints ) 
-{
-    if( startPoints . size () != endPoints . size ()) 
-        throw invalid_argument (" Start /end point lists differ in length ");
-
-    for(unsigned i = 0; i < startPoints . size (); i++ ) 
-    {
-        int start = startPoints [i], end = endPoints [i];
-        outgoing [ start ]. push_back (end);
-        outgoing [end ];
-    }
-}
-
-int Graph :: numOutgoing ( const int nodeID ) const {
-return adjacent ( nodeID ). size ();
-}
-const vector <int > & Graph :: adjacent (const int nodeID) const 
-{
-    map<int , vector <int > >::const_iterator i = outgoing.find(nodeID);
-    if(i == outgoing . end ()) 
-    {
-        throw invalid_argument (" Invalid node ID");
-    }
-    return i->second;
-}
 
 int main()
 {
+    cout<<"\nGrafo creado en C++: \n";
+    vector<int>inicio = {1,1,1,5,5,4};
+    vector<int>final = {2,3,4,4,2,2};
+    grafo miGrafo(inicio,final);
 
+    cout<<"\nNodos con los que el nodo '1' inicia una arista direccionada hacia ellos: ";
+    vector<int> prueba = miGrafo.adyacente(1);
+    for(auto i=prueba.begin();i!=prueba.end();i++)
+        cout<<*i<<" ";
+    cout<<"\nNodos con los que el nodo '5' inicia una arista direccionada hacia ellos: ";
+    prueba = miGrafo.adyacente(5);
+    for(auto i=prueba.begin();i!=prueba.end();i++)
+        cout<<*i<<" ";
+    cout<<"\nNodos con los que el nodo '4' inicia una arista direccionada hacia ellos: ";
+    prueba = miGrafo.adyacente(4);
+    for(auto i=prueba.begin();i!=prueba.end();i++)
+        cout<<*i<<" ";
+    cout<<endl<<endl;
 }
