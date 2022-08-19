22cb49b433efa4303c0a2f84b88687bf289e97eb
SebastianZamalloa <104155286+SebastianZamalloa@users.noreply.github.com>
2022-08-19 13:40:27 -0500

    Update weak.cpp

--- functores_smartPointers/smartPointers/weak.cpp
+++ functores_smartPointers/smartPointers/weak.cpp
@@ -3,27 +3,25 @@
 using namespace std;
  
 weak_ptr<int> gw;
- 
+
+
 void observe()
 {
     cout << "gw.use_count() == " << gw.use_count() << "; ";
-    if (shared_ptr<int> spt = gw.lock()) {
+    if (shared_ptr<int> spt = gw.lock())
         cout << "*spt == " << *spt << '\n';
-    }
     else 
-    {
         cout << "gw is expired\n";
-    }
 }
  
 int main()
 {
+    cout<<endl;
     {
         auto sp = make_shared<int>(42);
         gw = sp;
- 
         observe();
     }
- 
     observe();
+    cout<<endl;
 }
